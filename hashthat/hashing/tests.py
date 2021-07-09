acf4e17cd0a47fcc9309ad352b5ded619efedeae
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-09 14:24:38 -0700

    All 7 tests passing

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -4,26 +4,26 @@ from .forms import HashForm
 import hashlib 
 from .models import Hash
 
-# class FunctionalTestCase(TestCase):
+class FunctionalTestCase(TestCase):
 
 
-#     def setUp(self):
-#         self.browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
 
-#     def test_there_is_homepage(self):
-#         self.browser.get('http://localhost:8000') # To go to the homepage
-#         self.assertIn('Enter hash here',self.browser.page_source)
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000') # To go to the homepage
+        self.assertIn('Enter hash here',self.browser.page_source)
 
-#     def test_hash_of_hello(self):
-#         self.browser.get('http://localhost:8000')
-#         text = self.browser.find_element_by_id('id_text') # selenium function that says go find something in the html that has the following id
-#         text.send_keys('hello') # entering text hello
-#         self.browser.find_element_by_name('submit').click() # submitting button
-#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text') # selenium function that says go find something in the html that has the following id
+        text.send_keys('hello') # entering text hello
+        self.browser.find_element_by_name('submit').click() # submitting button
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
-#     def tearDown(self):
-#         self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
 class UnitTestCase(TestCase):
 
