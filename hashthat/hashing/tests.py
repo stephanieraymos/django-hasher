b7a77f3997391f280ed3cc2b2473bf4b76f88828
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-09 13:48:47 -0700

    Created Hash object and running test

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from selenium import webdriver
 from .forms import HashForm
 import hashlib 
-
+from .models import Hash
 
 # class FunctionalTestCase(TestCase):
 
@@ -38,3 +38,12 @@ class UnitTestCase(TestCase):
     def test_hash_func_works(self):
         text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest() # hexdigest passes back string so we can do some assertion
         self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash) # asserting that the hashed string (2CF2...) is equal to text_hash
+
+    def test_hash_object(self):
+        hash = Hash() # gives us instance of hash object
+        hash.text = 'hello' # adding text and hash values to this new object
+        hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+        hash.save() # saving to db
+        pulled_hash = Hash.objects.get(hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824') # finding in db a hash object with this exact hash
+        self.assertEqual(hash.text, pulled_hash) # asserting that text in hash.text is equal to pulled_hash
+
