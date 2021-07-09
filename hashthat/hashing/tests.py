94c496fc7e2d2a71dafc8b3afcff4b5973414d56
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-09 13:42:48 -0700

    All 3 tests passing

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from selenium import webdriver
 from .forms import HashForm
+import hashlib 
 
 
 # class FunctionalTestCase(TestCase):
@@ -19,7 +20,7 @@ from .forms import HashForm
 #         text = self.browser.find_element_by_id('id_text') # selenium function that says go find something in the html that has the following id
 #         text.send_keys('hello') # entering text hello
 #         self.browser.find_element_by_name('submit').click() # submitting button
-#         self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
 #     def tearDown(self):
 #         self.browser.quit()
@@ -33,3 +34,7 @@ class UnitTestCase(TestCase):
     def test_hash_form(self):
         form = HashForm(data = {'text':'hello'})
         self.assertTrue(form.is_valid)
+
+    def test_hash_func_works(self):
+        text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest() # hexdigest passes back string so we can do some assertion
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash) # asserting that the hashed string (2CF2...) is equal to text_hash
