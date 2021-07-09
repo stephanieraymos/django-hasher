cb725c163ddd30dfc7534030e97520e69a51e9ba
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-09 13:13:33 -0700

    Wrote test for hasher

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -10,9 +10,16 @@ class FunctionalTestCase(TestCase):
 
 
     def test_there_is_homepage(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('install',self.browser.page_source)
+        self.browser.get('http://localhost:8000') # To go to the homepage
+        self.assertIn('Enter hash here',self.browser.page_source)
 
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text') # selenium function that says go find something in the html that has the following id
+        text.send_keys('hello') # entering text hello
+        self.browser.find_element_by_name('submit').click() # submitting button
+        self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
 
     def tearDown(self):
         self.browser.quit()
+
