c62efe551a1bdfb470cf3b7f408ee551582c7fcb
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-09 14:15:34 -0700

    Created function for saving hash

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -39,18 +39,19 @@ class UnitTestCase(TestCase):
         text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest() # hexdigest passes back string so we can do some assertion
         self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash) # asserting that the hashed string (2CF2...) is equal to text_hash
 
-    def test_hash_object(self):
+    def saveHash(self):
         hash = Hash() # gives us instance of hash object
         hash.text = 'hello' # adding text and hash values to this new object
         hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
-        hash.save() # saving to db
+        hash.save() # saving to db        
+        return hash
+
+    def test_hash_object(self):
+        hash = self.saveHash()
         pulled_hash = Hash.objects.get(hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824') # finding in db a hash object with this exact hash
         self.assertEqual(hash.text, pulled_hash.text) # asserting that text in hash.text is equal to pulled_hash
 
     def test_viewing_hash(self):
-        hash = Hash() # gives us instance of hash object
-        hash.text = 'hello' # adding text and hash values to this new object
-        hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
-        hash.save() # saving to db       
+        hash = self.saveHash()      
         response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
         self.assertContains(response,'hello')
